031f20851a58766436d1c395a14cd21796f916f5
DeLucaJ <delucaj96@gmail.com>
2018-12-26 15:25:19 -0800

    added grammar dict

--- generate.py
+++ generate.py
@@ -1,10 +1,22 @@
 import sys
 import re
+import random
 from string import Template
 
 # Constants
 GRAMMAR_TERM = r"\S*"	# Regular Expression for terms in grammar
-NON_TERMINAL = r"\$\$*"	# Regular Expression for non_terminal in template form
+NON_TERMINAL = r"\$\S*"	# Regular Expression for non_terminal in template form
+
+class grammar(dict):
+    '''
+    Modification of dictionary
+	Allows the return of a random single item in a key list.
+    '''
+    def __getitem__(self, key):
+        return random.choice(super(rdict, self).__getitem__(key))
+
+    def getList(self, key):
+        return super(rdict, self).__getitem__(key)
 
 def grammar_to_template(line):
 	'''
@@ -19,10 +31,10 @@ def parse(file):
 	Parses a file and outputs the grammar as a dictionary
 
 	@param file:	an open file
-	@return: 		a dictionary(grammar) that contains a start_key variable
+	@return: 		a dictionary(grammar) and a string (start_key) variable
 	'''
 	grammar = dict()
-	grammar["start_key"] = ""
+	start_key = ""
 
 	#convert each line of the file into a dictionary entry
 	lines = file.readlines()
@@ -39,8 +51,8 @@ def parse(file):
 		# add key to dictionary
 		# If its the first, make it the start_key
 		key = item[0].strip()
-		if not grammar["start_key"]:
-			grammar["start_key"] = key
+		if not start_key:
+			start_key = key
 		if key not in gramar.keys():
 			grammar[key] = list()
 
@@ -53,16 +65,22 @@ def parse(file):
 			value = value.strip()
 			grammar[key].append(value)
 
-	return grammar
+	return (grammar, start_key)
 
-def interpret(grammar):
+def interpret(grammar, start_key):
 	'''
 	Takes a dictionary grammar and turns it into a string of Inform7 code
 
 	@param grammar: a dictionary grammar (should have start_key)
 	@return: 		a string to be placed in Inform7
 	'''
-	pass
+	# initialize profile as the first line of the grammar
+	profile = grammar[start_key].copy()
+
+	# Loop until there are no more non-terminals
+	while re.search(NON_TERMINAL, profile)
+		#likely needs mitches structure
+		t = Template(profile).substitute(grammar)
 
 def main():
 	'''
@@ -100,7 +118,7 @@ def main():
 
 	# turn grammars into profiles
 	for grammar in grammars
-		profiles.append(interpret(grammar))
+		profiles.append(interpret(grammar[0], grammar[1]))
 
 	# put profiles into and Inform7 file
 
