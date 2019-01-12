58063dffa1458b03aae984940fa880198d1bc666
DeLucaJ <delucaj96@gmail.com>
2019-01-12 15:49:09 -0800

    finished highlighting for content files. some progress on new parser

--- generate.sh
+++ generate.sh
@@ -1,10 +1,15 @@
-dir='./profiles/'
 script='./python/generate.py'
+grammar='./content/grammar.txt'
+assets='./content/asset_lib.txt'
+template='./template.txt'
 output='output.txt'
+#dir='./profiles/'
 
 rm $output
 
-for file in $dir*_grammar.txt;
-do
-	python $script $file >> $output
-done	
+#for file in $dir*_grammar.txt;
+#do
+
+python $script $grammar $assets $template >> $output
+
+#done	
--- python/generate_new.py
+++ python/generate_new.py
@@ -3,6 +3,9 @@ import re
 import random
 from string import Template
 
+#Constants
+content_dir = "./content/"
+
 def parse_grammar(gfile):
 	pass
 
@@ -16,7 +19,58 @@ def fill_template(tfile, fgrammar):
 	pass
 
 def main():
-	pass
+	g = ""
+	a = ""
+	t = ""
+
+	# process arguments PROBABLY NEEDS WORK
+	if len(sys.argv) != 4:
+		print("Improper number of files. Looking for defaults,'template.txt', 'asset_lib.txt', and 'grammar.txt'")
+		g = content_dir + "grammar.txt"
+		a = content_dir + "asset_lib.txt"
+		t = content_dir + "template.txt"
+	else: 	
+		# add all arguments to list of files
+		g = sys.argv[1]
+		a = sys.argv[2]
+		t = sys.argv[3]
+		
+
+	# parse file into grammar
+	try:
+		gfile = open(g, "r")
+	except:
+		print("Grammar definition '" + g + "' not found", file=sys.stderr)
+		sys.exit()
+
+	grammar = parse_grammar(gfile)
+	gfile.close()
+
+	# parse asset library
+	try:
+		afile = open(a)
+	except:
+		print("Asset Library '" + a + "' not found.", file=sys.stderr)
+		sys.exit()
+
+	assets = parse_assets(afile)
+	afile.close()
+
+	# fill grammars with asset library
+	fgrammar = (fill_grammar(grammar, assets))
+
+	# fill template
+	try:
+		tfile = open(t)
+	except:
+		print("Template file '" + t + "' not found.", file=sys.stderr)
+		sys.exit()
+
+	output = fill_template(tfile, fgrammar)
+	tfile.close
+
+	# output the string
+	print(output)
 
 if __name__ == '__main__':
 	main()
