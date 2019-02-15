7c57ad295474bed4e2368457c58e86f4fb99bd5b
DeLucaJ <delucaj96@gmail.com>
2019-02-15 01:30:09 -0800

    Working parse pipeline

--- python/generate_new.py
+++ python/generate_new.py
@@ -1,22 +1,157 @@
 import sys
 import re
 import random
-from string import Template
+import string
 
 #Constants
-content_dir = "./content/"
+CONTENT_DIR = "./content/"
+GRAMMAR_TERM = r"<\S*>"	# Regular Expression for terms in grammar
+NON_TERMINAL = r"\$\S*"	# Regular Expression for non_terminal in template form
+
+class gdict(dict):
+    '''
+    Modification of dictionary
+	- Allows the return of a random single item in a key list.
+	- Useful for random generation using string templates
+    '''
+    def __getitem__(self, key):
+        return random.choice(super(gdict, self).__getitem__(key))
+
+    def getList(self, key):
+        return super(gdict, self).__getitem__(key)
+
+def grammar_to_template(line):
+	'''
+	Takes in a grammar string and makes it into string template format
+	
+	@param line:	a string in grammar format
+	
+	@return tline: 	line in template format
+	'''
+	tline = line[:]
+	to_replace = re.findall(GRAMMAR_TERM, line)
+	
+	for item in to_replace:
+		tform = '$' + item[1:-1]
+		tline = tline.replace(item, tform)
+
+	return tline
 
 def parse_grammar(gfile):
-	pass
+	'''
+	Parses a file and outputs the grammar as a dictionary
+
+	@param file:	an open file
+	@return: 		a grammar dictionary and a string (start_key) variable
+	'''
+	grammar = gdict()
+	start_key = ""
+
+	line = gfile.readline()
+	while (line != ""):
+		line = line.strip()
+
+		# Check format
+		if ("=>" not in line):
+			print("Grammar imperly formatted", file=sys.stderr)
+			sys.exit(1)
+
+		# split key and values
+		item = line.split("=>")
+
+		# add key to grammar
+		# 	If it is the first key, make it the start_key
+		key = item[0].strip()
+		if not start_key:
+			start_key = key
+		if key not in grammar.keys():
+			grammar[key] = list()
+
+		# delimt by '|'
+		values = grammar_to_template(item[1].strip())
+		values = values.split("|")
+
+		# add values to grammar
+		for value in values:
+			value = value.strip()
+			grammar.getList(key).append(value)
+
+		line = gfile.readline()
+
+	return (grammar, start_key)
 
 def parse_assets(afile):
-	pass
+	'''
+	Parses a file and outputs the assets as a dicitonary
 
-def fill_grammar(grammar, assets):
-	pass
+	@param file:	an open file
+	@return: 		a asset dictionary
+	'''
+	assets = dict()
 
-def fill_template(tfile, fgrammar):
-	pass
+	line = afile.readline()
+	while (line != ""):
+		line = line.strip()
+
+		# check format
+		if (":" not in line):
+			print("Asset Lib improperly formated")
+			sys.exit()
+		
+		# split key and values
+		item = line.split(":")
+
+		#add key to assets
+		key = item[0].strip()
+		assets[key] = item[1].strip()
+
+		line = afile.readline()
+
+	return assets
+
+def interpret_grammar(grammar, start_key, assets):
+	'''
+	Takes a dictionary grammar and turns it into a string of Inform7 code
+
+	@param grammar: a grammar dictionary
+	@param start_key: the beginning key in the grammar
+	@param assets: an asset dictionary
+
+	@return: a string of output 
+	'''
+	output = grammar[start_key][:]
+	
+	while re.search(NON_TERMINAL, output):
+		output = string.Template(output).substitute(grammar)
+
+	output = output.split()
+
+	# replace terminals (hopefully \n delimitted) with corresponding assets 
+	replaced = ""
+	for item in output:
+		replaced += assets[item] + "\n"
+
+	return replaced
+
+def fill_template(tfile, anchor):
+	'''
+	Fills the template using 
+
+	@param tfile: the template file
+	@param grammar: the grammar with asset keys
+	@param assets: the asset dictionary
+
+	@return filled template
+	'''
+	output = ""
+
+	# put entire tfile into output
+	output = tfile.read()
+
+	# put the grammar into the template
+	output = string.Template(output).substitute(anchor)
+
+	return output	
 
 def main():
 	g = ""
@@ -25,10 +160,11 @@ def main():
 
 	# process arguments PROBABLY NEEDS WORK
 	if len(sys.argv) != 4:
-		print("Improper number of files. Looking for defaults,'template.txt', 'asset_lib.txt', and 'grammar.txt'")
-		g = content_dir + "grammar.txt"
-		a = content_dir + "asset_lib.txt"
-		t = content_dir + "template.txt"
+		print("Improper number of files.")
+		print("Looking for 'template.txt', 'asset_lib.txt', and 'grammar.txt'")
+		g = CONTENT_DIR + "grammar.txt"
+		a = CONTENT_DIR + "asset_lib.txt"
+		t = CONTENT_DIR + "template.txt"
 	else: 	
 		# add all arguments to list of files
 		g = sys.argv[1]
@@ -48,7 +184,7 @@ def main():
 
 	# parse asset library
 	try:
-		afile = open(a)
+		afile = open(a, "r")
 	except:
 		print("Asset Library '" + a + "' not found.", file=sys.stderr)
 		sys.exit()
@@ -56,17 +192,18 @@ def main():
 	assets = parse_assets(afile)
 	afile.close()
 
-	# fill grammars with asset library
-	fgrammar = (fill_grammar(grammar, assets))
-
 	# fill template
 	try:
-		tfile = open(t)
+		tfile = open(t, "r")
 	except:
 		print("Template file '" + t + "' not found.", file=sys.stderr)
 		sys.exit()
 
-	output = fill_template(tfile, fgrammar)
+	anchor = dict()
+
+	anchor["anchor"] = interpret_grammar(grammar[0], grammar[1], assets)
+
+	output = fill_template(tfile, anchor)
 	tfile.close
 
 	# output the string
